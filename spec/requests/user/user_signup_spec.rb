2b89cbaf2e460b5712536a2b0ecef97fa3f063b4
Sathiyaseelan <ssstarlet@gmail.com>
2017-02-19 01:21:10 +0530

    Refactored and fixed the test util classes

--- spec/requests/user/user_signup_spec.rb
+++ spec/requests/user/user_signup_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe 'User Signup request', type: :request do
 
   context 'public request with valid attributes' do
     it 'allow user to sign up' do
-      user_params = build_json_for :user
-      post_json users_path, params: user_params
+      user_params = build_json_for factory: :user, root: :user
+      post_json url: users_path, params: user_params
+
       expect(response.status).to eq 201
       expect(response_value('success')).to eq true
     end
--- spec/support/service_test_extenions.rb
+++ spec/support/service_test_extenions.rb
@@ -2,12 +2,14 @@
 module ServiceTestExtensions
 
   # To build json for the factory and merge additional hash if any
-  def build_json_for(factory_name,additional_hash={})
-    attributes_for(factory_name).merge(additional_hash).to_json
+  def build_json_for(factory:, root:, additional_data: {})
+    {root => attributes_for(factory)}.merge(additional_data).to_json
   end
-  def json_for(record,additional_hash={})
-    record.attributes.merge(additional_hash).to_json
+
+  def json_for(record,additional_data: {})
+    record.attributes.merge(additional_data).to_json
   end
+
   # To parse the json and retrieve the value
   def response_value(key)
     JSON.parse(response.body)[key.to_s]
@@ -15,16 +17,16 @@ module ServiceTestExtensions
 
   # to send post request with content type as json
   # it will accept additional params and headers
-  def post_json(url, options={})
-    post url, params: options[:params], headers: headers(options)
+  def post_json(url:, params: {}, headers: {})
+    post url, params: params, headers: headers(additional_headers: headers)
   end
 
   private
 
   # To construct the headers
-  def headers(options = {})
+  def headers(additional_headers:)
     headers = { "CONTENT_TYPE" => "application/json"}
-    headers.merge!(options[:headers]) if options[:headers]
+    headers.merge!(additional_headers) if additional_headers
     return headers
   end
 
