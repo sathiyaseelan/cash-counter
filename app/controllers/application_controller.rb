92c7ac386980513279506a6b2e82495860733233
Sathiyaseelan <ssstarlet@gmail.com>
2017-03-14 23:45:34 +0530

    added cors gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_cross_domain_headers
-    response.headers['Access-Control-Allow-Origin'] = "*"
-    response.headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, OPTIONS"
+    # response.headers['Access-Control-Allow-Origin'] = "*"
+    # response.headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, OPTIONS"
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,12 @@ module CashCounter
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.cache_store = :redis_store, ENV['REDIS_URL']+"/0/cache", { expires_in: 90.minutes }
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
